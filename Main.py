b5acf2edc6dc16f19b725f95bf695fbad6b546c7
Bcnwy <benconway@outlook.com>
2017-05-11 14:31:10 +0100

    update main file access/save
    Signed-off-by: B_cnwy <benconway@outlook.com>

--- Main.py
+++ Main.py
@@ -4,36 +4,33 @@ import serial
 import csv
 
 # serial port parameters
-_Port = 'COM6'
-_Baudrate = 57600
+_Port = 'COM5'
+_Baudrate = 9600
 # path = local root
 _file = ("{}-MLX.csv".format(datetime.now().date()))
 
 if __name__ == '__main__':
 
     serial = serial.Serial(port=_Port, baudrate=_Baudrate)
-    print _file
+    #print _file
     # make file
     with open(_file, 'ab') as csvFile:
         writer = csv.writer(csvFile, dialect='excel')
         # write a new row the the csv file
-        header = 'Sensor', 'Object', 'Ambient', 'Distance', 'Time'
+        header = 'Sensor', 'Object', 'Ambient', 'Distance', 'uM_Time', 'Time'
         writer.writerow([datetime.now().__str__()])
         writer.writerow(header)
     csvFile.close()
 
     # start of EPOCH time
     startTime = time()
-    i = 0
     while 1:
-
         read = serial.readline().strip()  # Read data
         rTime = time() - startTime  # get running time
         # format the sensor data
         d = read.split(',')
         d += {rTime.__str__()}
         print d
-        i += 1
         # open csv file and set a writer to access the file
         with open(_file, 'ab') as csvFile:
             writer = csv.writer(csvFile, dialect='excel')
