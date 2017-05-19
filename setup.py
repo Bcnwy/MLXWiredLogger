2e14f6299c81f119d5f58d92c6245cc54f8eafb2
B_cnwy <benconway@outlook.com>
2017-05-19 12:48:01 +0100

    updates

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MLXWiredLogger',
+    version='v1',
+    packages=[''],
+    url='',
+    license='GPLv3.0',
+    author='Ben Conway',
+    author_email='',
+    description=''
+)
