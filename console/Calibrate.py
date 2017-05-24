af2e82826a2d4576f42bc002f20b6b620962ccc8
B_cnwy <benconway@outlook.com>
2017-05-24 17:19:09 +0100

    Json input on command line

--- console/Calibrate.py
+++ console/Calibrate.py
@@ -13,38 +13,33 @@ _Baudrate = 9600
 
 
 class MLX:
-    global temp
-
     def __init__(self):
         ports = list(serial.tools.list_ports.comports())
         for com in ports:
-            print com
-        print "Select Com Port: "
+            print(com)
+        print("Select Com Port: ")
         comport = raw_input()
         _Port = "COM{}".format(int(comport))
         try:
             com = self.serial.Serial(port=_Port, baudrate=_Baudrate)
         except:
-            print "Failed to open serial port"
+            print("Failed to open serial port")
             pass
 
     @staticmethod
     def calibrate(self):
         global temp
-        for i in range(1,40 ):
+        for i in range(1, 40):
             try:
                 r = self.serial.readline().strip()  # Read data
                 read = json.loads(r)
             except:
                 pass
             temp[read['Sensor']] = read['obj']
-
-        print temp
-
+        print(temp)
 
 if __name__ == '__main__':
-
-    with open(_calFile, 'ab') as csvFile:
+    with open(_calFile, 'a') as csvFile:
         writer = csv.writer(csvFile, dialect='excel')
         # write a new row the the csv file
         writer.writerow([datetime.now().__str__()])
@@ -55,4 +50,5 @@ if __name__ == '__main__':
     while 1:
         rTime = time() - startTime  # get running tim
         mlx.calibrate()
+
     serial.close()
--- console/Main.py
+++ console/Main.py
@@ -2,39 +2,54 @@ from datetime import datetime
 from time import time
 import serial
 import csv
+import ijson
+import json
 
 # serial port parameters
 _Port = 'COM5'
 _Baudrate = 9600
 # path = local root
-_file = ("{}-MLX.csv".format(datetime.now().date()))
+_file = ("{}-MLX.txt".format(datetime.now().date()))
 
 if __name__ == '__main__':
 
-    serial = serial.Serial(port=_Port, baudrate=_Baudrate)
-    #print _file
+    serial_port = serial.Serial(port=_Port, baudrate=_Baudrate)
+    # print _file
     # make file
-    with open(_file, 'ab') as csvFile:
+    with open(_file, 'a') as csvFile:
         writer = csv.writer(csvFile, dialect='excel')
         # write a new row the the csv file
-        header = 'Sensor', 'Object', 'Ambient', 'Distance', 'uM_Time', 'Time'
-        writer.writerow([datetime.now().__str__()])
+        header = 'Sensor', 'Object', 'Ambient', 'DistanceUltra', 'DistanceTOF', 'uTime', 'Time'
+        # writer.writerow([datetime.now().__str__()])
         writer.writerow(header)
     csvFile.close()
 
     # start of EPOCH time
+    # global startTime
     startTime = time()
     while 1:
-        read = serial.readline().strip()  # Read data
+        try:
+            read = serial_port.readline().decode('utf-8').rstrip()  # Read data
+            #  format the sensor data
+            d = json.loads(str(read))
+            print(d)
+        except:
+            pass
+
         rTime = time() - startTime  # get running time
-        # format the sensor data
-        d = read.split(',')
-        d += {rTime.__str__()}
-        print d
+
         # open csv file and set a writer to access the file
-        with open(_file, 'ab') as csvFile:
+        with open(_file, 'a') as csvFile:
             writer = csv.writer(csvFile, dialect='excel')
             # write a new row the the csv file
-            writer.writerow(d)
+            for mTemp in d:
+                # print(mTemp)
+                writer.writerow([mTemp['Sensor'],
+                                mTemp['Obj'],
+                                mTemp['Amb'],
+                                mTemp['Dist'][0],
+                                mTemp['Dist'][1],
+                                mTemp['uTime'],
+                                rTime])
     csvFile.close()
     serial.close()
